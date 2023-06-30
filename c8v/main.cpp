bab74351988e6db235852056bdac5ca93c4635e1
dmamills <d.mills@vehikl.com>
2023-06-30 15:13:39 -0400

    Initial Commit

---
+++ c8v/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c8v
+//
+//  Created by mills on 2023-06-30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
